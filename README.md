fcb514147d1f5948e5ea11f56237754ff24c33d4
Vortex842 <nahuelat.8@gmail.com>
2024-01-15 18:32:21 -0300

    Add AppBar Custom and update README

--- README.md
+++ README.md
@@ -3,6 +3,22 @@ Anota los productos de tu compra de forma limpia. Puedes visualizarlos ya sea si
 
 
 ## TODO
+* Agragar tema light / dark
+    * Colores:
+        * Page Background: FFFFFF / 1D1B20
+        * Cards Color: F3EDF7 / 211F26
+        * Button Color: ECE6F0 / 2B2930
+        * Edit Foreground: 0B4B01
+        * Edit Background: 59FF56
+        * Delete Foreground: 780000
+        * Delete Background: FF5656
+        * Select Foreground: 430078
+        * Select Background: BB77FF
+        * Foreground Primary Color (in light mode): 1D1B20
+        * Foreground Primary Color (in dark mode): D2C2EB
+        * Icon Foreground: 21005D
+
+    * Font Moncerrat
 * Implementar
     * Clean Architecture
     * Principios SOLID
--- lib/widgets/shopping_app_bar.dart
+++ lib/widgets/shopping_app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lucide_icons/lucide_icons.dart';
 
 class ShoppingAppBar extends StatelessWidget implements PreferredSizeWidget {
   const ShoppingAppBar({super.key});
@@ -8,15 +9,28 @@ class ShoppingAppBar extends StatelessWidget implements PreferredSizeWidget {
     return LayoutBuilder(
       builder: (_, bordes) => SizedBox.fromSize(
         size: Size.fromHeight(bordes.maxHeight),
-        child: const Stack(
-          children: [
-            Center(
-              child: Text(
-                "Shopping List",
-                style: TextStyle(fontSize: 25),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    LucideIcons.alignLeft,
+                    size: 25,
+                  ),
+                ),
               ),
-            ),
-          ],
+              const Center(
+                child: Text(
+                  "Shopping List",
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
