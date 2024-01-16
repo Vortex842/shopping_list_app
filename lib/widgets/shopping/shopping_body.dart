4e3270a3291042988c96d49ef702e5458df1d771
Vortex842 <nahuelat.8@gmail.com>
2024-01-15 22:39:03 -0300

    Create the body and button custom models

--- lib/widgets/shopping/shopping_body.dart
+++ lib/widgets/shopping/shopping_body.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
+import 'shopping_button.dart';
+
 class ShoppingBody extends StatelessWidget {
   const ShoppingBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.red);
+    return Container(
+      color: Colors.red,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Wrap(
+            direction: Axis.horizontal,
+            runSpacing: 10,
+            children: List.generate(
+              10,
+              (index) => const ShoppingButton(),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/shopping/shopping_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShoppingButton extends StatelessWidget {
+  const ShoppingButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(height: 60, color: Colors.blue);
+  }
+}
