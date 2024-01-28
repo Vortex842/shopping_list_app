f98f24d8f9f3383615da697e9c081cdf4ce72f8b
Vortex842 <nahuelat.8@gmail.com>
2024-01-28 15:46:06 -0300

    Fixing Bottom Bar

--- lib/presentation/widgets/shopping/bottom_bar/shopping_bottom.dart
+++ lib/presentation/widgets/shopping/bottom_bar/shopping_bottom.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../providers/dark_mode_provider.dart';
+import '../../../providers/product_list_provider.dart';
 import '../../../references/references.barrel.dart';
-import 'buttons/buttons_bottombar.barrel.dart';
+import 'bottombar_buttons/bottom_bar_buttons.dart';
 
 class ShoppingBottom extends ConsumerWidget {
   final double maxHeight;
@@ -13,31 +14,21 @@ class ShoppingBottom extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final isDark = ref.watch(isDarkMode);
+    final isAnyChecked = ref.watch(isAnyCheckedProvider);
 
     return Container(
+      width: double.infinity,
       height: maxHeight,
       decoration: ref.bottomDecoration(isDark),
-      child: const Center(
-        child: BottomBarButtons(),
+      child: const Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 15,
+        ),
+        child: Align(
+          alignment: Alignment.center,
+          child: BottomBarButtons(),
+        ),
       ),
-    );
-  }
-}
-
-class BottomBarButtons extends StatelessWidget {
-  const BottomBarButtons({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return const Wrap(
-      spacing: 10,
-      crossAxisAlignment: WrapCrossAlignment.center,
-      children: [
-        AddProductButton(),
-        SearchProductButton(),
-      ],
     );
   }
 }
