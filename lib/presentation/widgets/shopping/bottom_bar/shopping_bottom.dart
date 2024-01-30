da3da26a2bee16c6999b5339b88e56409daab600
Vortex842 <nahuelat.8@gmail.com>
2024-01-29 21:15:44 -0300

    Implement onAddEdit provider and references

--- lib/presentation/widgets/shopping/bottom_bar/shopping_bottom.dart
+++ lib/presentation/widgets/shopping/bottom_bar/shopping_bottom.dart
@@ -1,32 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../../providers/dark_mode_provider.dart';
-import '../../../providers/product_list_provider.dart';
+import '../../../providers/providers.barrel.dart';
 import '../../../references/references.barrel.dart';
 import 'bottombar_buttons/bottom_bar_buttons.dart';
 
 class ShoppingBottom extends ConsumerWidget {
-  final double maxHeight;
-
-  const ShoppingBottom({required this.maxHeight, super.key});
+  const ShoppingBottom({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final isDark = ref.watch(isDarkMode);
     final isAnyChecked = ref.watch(isAnyCheckedProvider);
+    final onAddEdit = ref.watch(onAddEditProvider);
 
     return Container(
       width: double.infinity,
-      height: maxHeight,
+      height: ref.bottomHeight(onAddEdit),
       decoration: ref.bottomDecoration(isDark),
-      child: const Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: 15,
-        ),
-        child: Align(
-          alignment: Alignment.center,
-          child: BottomBarButtons(),
+      child: ClipRRect(
+        borderRadius: ref.cardRadius,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 15,
+          ),
+          child: Align(
+            // TODO: Create Actions: 'selected' and 'Accept or Cancel'
+            alignment: !isAnyChecked ? Alignment.center : Alignment.centerLeft,
+            child: !isAnyChecked
+                ? const BottomBarButtons()
+                : Container(
+                    width: 100,
+                    color: Colors.red,
+                  ),
+          ),
         ),
       ),
     );
