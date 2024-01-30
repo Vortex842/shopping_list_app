03a939ae1c8176e516e0f3acc4d85fff6587f8f4
Vortex842 <nahuelat.8@gmail.com>
2024-01-30 00:34:50 -0300

    Make Shopping Text Editable

--- lib/presentation/widgets/shopping/bottom_bar/add_edit_section.dart
+++ lib/presentation/widgets/shopping/bottom_bar/add_edit_section.dart
@@ -1,15 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shopping_list_app/presentation/references/references.barrel.dart';
 
-class AddEditSection extends StatelessWidget {
+import '../../../providers/dark_mode_provider.dart';
+
+class AddEditSection extends ConsumerWidget {
   const AddEditSection({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Container(
       // width: 100,
-      color: Colors.green,
+      // color: Colors.green,
+      child: const Center(
+        child: ShoppingTextEditable(),
+      ),
+    );
+  }
+}
+
+class ShoppingTextEditable extends ConsumerWidget {
+  const ShoppingTextEditable({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isDark = ref.watch(isDarkProvider);
+
+    return SizedBox(
+      width: ref.editableTextWidth,
+      child: InputDecorator(
+        decoration: InputDecoration(
+          isCollapsed: true,
+          fillColor: ref.editableTextColor(isDark),
+          filled: true,
+          border: const UnderlineInputBorder(),
+        ),
+        child: EditableText(
+          controller: TextEditingController(),
+          focusNode: FocusNode(),
+          style: ref.normalText().copyWith(height: 2),
+          cursorHeight: 22,
+          cursorOffset: const Offset(0, 5),
+          cursorColor: Colors.black87,
+          backgroundCursorColor: Colors.transparent,
+        ),
+      ),
     );
   }
 }
