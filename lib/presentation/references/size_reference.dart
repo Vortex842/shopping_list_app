4c719627852f8c042ff835dfe4b59b9a154daef5
Vortex842 <nahuelat.8@gmail.com>
2024-01-30 15:51:59 -0300

    Adding best editable text heights

--- lib/presentation/references/size_reference.dart
+++ lib/presentation/references/size_reference.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 extension ShoppingSize on WidgetRef {
@@ -6,5 +7,15 @@ extension ShoppingSize on WidgetRef {
   double get _bottomActiveHeight => 160;
   double bottomHeight(bool isActive) =>
       isActive ? _bottomActiveHeight : _bottomNormalHeight;
-  double get editableTextWidth => 180;
+  double get _editableProductWidth => 180;
+  double get _editableAmountWidth => 45;
+  double get _editablePriceWidth => 90;
+  double editableTextWidth(TextInputType textType) =>
+      textType == TextInputType.text
+          ? _editableProductWidth
+          : textType == TextInputType.phone
+              ? _editableAmountWidth
+              : textType == TextInputType.number
+                  ? _editablePriceWidth
+                  : 0;
 }
