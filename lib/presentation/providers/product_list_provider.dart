8190f1438a0eadd7b2c38ec0d79d411a5433c01a
Vortex842 <nahuelat.8@gmail.com>
2024-01-30 17:38:47 -0300

    Initial list product is empty

--- lib/presentation/providers/product_list_provider.dart
+++ lib/presentation/providers/product_list_provider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:uuid/uuid.dart';
 
 import '../../data/domain/entities/product.dart';
 
@@ -23,48 +22,7 @@ final onMultiSelectProvider = StateProvider<bool>((ref) {
 class ProductNotifier extends StateNotifier<List<Product>> {
   ProductNotifier()
       : super(
-          [
-            Product(
-                id: const Uuid().v4(), name: "Fideos", price: 856, amount: 12),
-            Product(
-                id: const Uuid().v4(), name: "Arroz", price: 2366, amount: 2),
-            Product(
-                id: const Uuid().v4(), name: "Atun", price: 85136, amount: 8),
-            Product(
-                id: const Uuid().v4(),
-                name: "Carne 1kg",
-                price: 7899,
-                amount: 12),
-            Product(
-                id: const Uuid().v4(), name: "Manteca", price: 856, amount: 5),
-            Product(
-                id: const Uuid().v4(), name: "Arroz", price: 5151, amount: 0),
-            Product(id: const Uuid().v4(), name: "Atun", price: 856, amount: 2),
-            Product(
-                id: const Uuid().v4(),
-                name: "Carne 1kg",
-                price: 1472,
-                amount: 12),
-            Product(
-                id: const Uuid().v4(),
-                name: "Manteca",
-                price: 25893,
-                amount: 0),
-            Product(
-                id: const Uuid().v4(), name: "Azucar", price: 856, amount: 1),
-            Product(
-                id: const Uuid().v4(),
-                name: "Carne 1kg",
-                price: 2023610,
-                amount: 12),
-            Product(
-                id: const Uuid().v4(),
-                name: "Manteca",
-                price: 800236,
-                amount: 6),
-            Product(
-                id: const Uuid().v4(), name: "Azucar", price: 856, amount: 1),
-          ],
+          [],
         );
 
   void addProduct(Product product) {
