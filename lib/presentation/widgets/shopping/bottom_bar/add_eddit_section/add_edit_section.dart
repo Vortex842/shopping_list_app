f92f10621a28e35097e8a42fe7f4da74e446e13f
Vortex842 <nahuelat.8@gmail.com>
2024-02-08 09:31:03 -0300

    Major of Add/Edit section

--- lib/presentation/widgets/shopping/bottom_bar/add_eddit_section/add_edit_section.dart
+++ lib/presentation/widgets/shopping/bottom_bar/add_eddit_section/add_edit_section.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shopping_list_app/presentation/widgets/shopping/bottom_bar/add_eddit_section/text_sections.dart';
 
-import '../../../../providers/providers.barrel.dart';
 import 'close_product_button.dart';
 import 'done_product_button.dart';
 
@@ -16,20 +15,19 @@ class AddEditSection extends ConsumerWidget {
     return Align(
       alignment: Alignment.centerLeft,
       child: GestureDetector(
-        onTap: () {
-          ref.read(focusProviders).forEach((focus) => focus.dispose());
-        },
+        onTap: () {},
         child: const Row(
           children: [
             Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  NameTextSection(),
-                  AmountTextSection(),
-                  PriceTextSection(),
-                ],
+              child: SingleChildScrollView(
+                child: Wrap(
+                  runSpacing: 20,
+                  children: [
+                    NameTextSection(),
+                    AmountTextSection(),
+                    PriceTextSection(),
+                  ],
+                ),
               ),
             ),
             Column(
