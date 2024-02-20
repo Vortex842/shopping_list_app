0e37541c141362c38ef6b87302467747a4df8812
Vortex842 <nahuelat.8@gmail.com>
2024-02-20 20:02:16 -0300

    Has visible shopping bottom

--- lib/presentation/widgets/shopping/body/shopping_body_animated.dart
+++ lib/presentation/widgets/shopping/body/shopping_body_animated.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:shopping_list_app/presentation/providers/on_change_states/on_side_menu_active_provider.dart';
-import 'package:shopping_list_app/presentation/widgets/shopping/body_transfrom.dart';
-import 'package:shopping_list_app/presentation/widgets/shopping/shopping.barrel.dart';
+
+import '/presentation/providers/on_change_states/on_side_menu_active_provider.dart';
+import '/presentation/widgets/shopping/body_transfrom.dart';
+import '/presentation/widgets/shopping/shopping.barrel.dart';
 
 class ShoppingAnimatedBody extends HookConsumerWidget {
   const ShoppingAnimatedBody({
@@ -34,15 +35,12 @@ class ShoppingAnimatedBody extends HookConsumerWidget {
       animation: controller,
       builder: (context, child) => BodyTransform(
         value: controller.value,
-        body: Column(
+        body: const Column(
           children: [
-            const Expanded(
+            Expanded(
               child: ShoppingBody(),
             ),
-            Visibility(
-              visible: !onSideMenuActive,
-              child: const ShoppingBottom(),
-            ),
+            ShoppingBottom(),
           ],
         ),
       ),
