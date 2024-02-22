4db3911d1a5882c6067d28759934083208df3f31
Vortex842 <nahuelat.8@gmail.com>
2024-02-22 11:37:36 -0300

    Create ProductAdapter

--- lib/data/classes/product class/product.dart
+++ lib/data/classes/product class/product.dart
@@ -1,9 +1,22 @@
-class Product {
+import 'package:hive/hive.dart';
+
+part 'product.g.dart';
+
+@HiveType(typeId: 1)
+class Product extends HiveObject {
+  @HiveField(0)
   final String id;
+
+  @HiveField(1)
   final String name;
+
+  @HiveField(2)
   final num price;
+
+  @HiveField(3)
   final int amount;
 
+  @HiveField(4)
   final bool isChecked;
 
   Product({
